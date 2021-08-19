df6fd9d1aae3dbb90533a7720196466708bcc026
SashaJson <sashamiller666@gmail.com>
2021-08-19 19:29:15 +0300

    small fix in index.js and create TODO for changed which need will make

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
+'use strict';
+
+// TODO REFACTOR THIS MODULE
+
 const express = require('express');
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
-const exphbs = require("express-handlebars");
-const todoRoutes = require("./routes/todos");
+const mongoose = require('mongoose');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos');
 const Handlebars = require('handlebars');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
-const bodyParser = require('body-parser');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
 const hbs = exphbs.create({
     defaultLayout: 'main',
-    extname: "hbs",
+    extname: 'hbs',
     handlebars: allowInsecurePrototypeAccess(Handlebars)
-})
+});
 
-//app.use(cors());
-app.engine("hbs", hbs.engine) // registered engineer with key hbs
-app.set("view engine", "hbs")
-app.set("views", "views")
+app.engine('hbs', hbs.engine); // registered engineer with key hbs
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use(express.urlencoded({extended: true}));
-//app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(todoRoutes);
 
 async function start() {
@@ -36,16 +37,15 @@ async function start() {
                 useFindAndModify: false,
                 useUnifiedTopology: true
             }
-        )
+        );
 
         app.listen(PORT, () => {
-            console.log("Server has been started...")
-        })
+            console.log('Server has been started...');
+        });
 
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
 }
 
-
-start()
+start();
